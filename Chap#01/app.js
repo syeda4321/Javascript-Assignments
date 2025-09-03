deac64294d2b2051f71b94d70997386c3cde614e
syeda4321 <aishaaqeel98@gmail.com>
2025-09-03 21:03:54 +0500

    done

---
+++ Chap#01/app.js
@@ -0,0 +1,17 @@
+// Chapter no 1
+
+// Qno1: Write a script to greet your website visitor using JS alert box?
+ alert("Hello! Users");
+
+// Qno2: Write a script to display following message on your web page?
+ alert("Error! please enter a valid password");
+
+//  Qno3: Write a script to display following message on your web page: (Hint : Use line break)?
+ alert("Welcome to JS land... \nHappy Coding!...");
+
+// Qno4: Write a script to display following messages in sequence?
+ alert("Welcome to JS land...");
+ alert("Happy Coding \n Prevents this page from creating additional dialogs")
+
+// Qno5: Generate the following message through browser’s developer console?
+alert("Hello...I can run JS through my web browser console")
