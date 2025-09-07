8191c15d79ded940db474303ad6078b4e345f5c5
syeda4321 <aishaaqeel98@gmail.com>
2025-09-07 20:43:04 +0500

    done

---
+++ Chap#04/app.js
@@ -0,0 +1,46 @@
+// Chapter no 4
+
+// Qno1:Declare 3 variables in one statement?
+
+let firstName
+let lastName
+let age
+
+// Qno2:Declare 5 legal & 5 illegal variable names?
+
+// LEGAL  VARIABLES
+
+let name
+let lastYear
+let last_qualification
+let $bithYear
+let dob321
+
+// ILLEGAL VARIABLES
+
+let ababab_
+let $abcd
+let $123 
+let abacs
+let 543student
+
+// Qno:3Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+
+document.write ("<h1>"+ "“Rules for naming JS variables”" +"</h1>")
+
+document.write ("Variables name can only contain..digit,letters,and Underscore (_)"+ "</br>"+ "For example $my_1stVariable"+"</br>")
+
+document.write ("Variables must begin with a..$,_,or name"+ "</br>" + "For example $name, _name or name")
+
+document.write ("Variable names are case sensitive"+"</br>")
+
+document.write ("Variable name should not be JS keywords")
